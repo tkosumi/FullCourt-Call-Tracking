683b67de736dc9c0a2128dece1c7ca10307ffc81
tkosumi <tkosumi@gmail.com>
2012-07-24 15:41:34 +0900

    modified:   handle_incoming_call.php

--- handle_incoming_call.php
+++ handle_incoming_call.php
@@ -3,7 +3,7 @@
 	header('Content-type: text/xml');
 ?>
 <Response>
-	<Dial callbackUrl="http://dev.fullcourt.co/FullCourt-Call-Tracking/record_call.php" method="GET">
+	<Dial callbackUrl="http://dev.fullcourt.co/FullCourt-Call-Tracking/record_call.php" method="GET" callerId="815058381227">
 		<Number><?php echo(AGENT_NUMBER);?></Number>
 	</Dial>
 </Response>
