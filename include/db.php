e7e9af73916f7077853db595a1cf14e723f93d58
tkosumi <tkosumi@gmail.com>
2012-07-24 14:33:06 +0900

    modified:   include/db.php

--- include/db.php
+++ include/db.php
@@ -9,49 +9,30 @@
 		}
 
 		function init() {
-			$this->db->exec('CREATE  TABLE IF NOT EXISTS calls ("CallSid" TEXT PRIMARY KEY  NOT NULL  UNIQUE , "DateCreated" DATETIME, "ToCountry" TEXT, "ToZip" TEXT, "ToState" TEXT, "ToCity" TEXT, "FromCountry" TEXT, "FromZip" TEXT, "ApiVersion" TEXT, "Direction" TEXT, "CallStatus" TEXT, "AccountSid" TEXT,  "CallTo" TEXT, "CallFrom" TEXT, "Status" TEXT, "StartTime" DATETIME, "EndTime" DATETIME, "Duration" INTEGER, "FromCity" TEXT, "FromState" TEXT, "DialCallSid" TEXT, "DialCallStatus" TEXT, "DialCallDuration" INTEGER, "RecordingUrl" TEXT);');
+			$this->db->exec('CREATE  TABLE IF NOT EXISTS calls ("CallSid" TEXT PRIMARY KEY  NOT NULL  UNIQUE , "DateCreated" DATETIME, "Direction" TEXT, "CallStatus" TEXT, "CallTo" TEXT, "CallFrom" TEXT, "Status" TEXT, "StartTime" DATETIME, "EndTime" DATETIME, "Duration" INTEGER,  "DialCallSid" TEXT, "DialCallStatus" TEXT, "DialCallDuration" INTEGER);');
 		}
 	
 		function save_call() {
-			//http://www.twilio.com/docs/api/twiml/twilio_request#synchronous-request-parameters
-			$CallSid = $_REQUEST['CallSid'];
-			$AccountSid=$_REQUEST['AccountSid'];
+			//https://www.fullcourt.co/ja/docs/PhoneXML/request
+			$CallSid = $_REQUEST['CallUUID'];
 			$CallFrom=$_REQUEST['From'];
 			$CallTo=$_REQUEST['To'];
 			$CallStatus=$_REQUEST['CallStatus'];
-			$ApiVersion=$_REQUEST['ApiVersion'];
 			$Direction=$_REQUEST['Direction'];
 
-			if (isset($_REQUEST['FromCity'])){
-				$FromCity=$_REQUEST['FromCity'];
-				$FromState=$_REQUEST['FromState'];
-				$FromZip=$_REQUEST['FromZip'];
-				$FromCountry=$_REQUEST['FromCountry'];
-			} else {
-				$FromCity="";
-				$FromState="";
-				$FromZip="";
-				$FromCountry="";
-			}
-			$ToCity=$_REQUEST['ToCity'];
-			$ToState=$_REQUEST['ToState'];
-			$ToZip=$_REQUEST['ToZip'];
-			$ToCountry=$_REQUEST['ToCountry'];
 
-			$stmt = $this->db->prepare('INSERT INTO calls (DateCreated,CallSid,AccountSid,CallFrom,CallTo,CallStatus,ApiVersion,Direction,FromCity,FromState,FromZip,FromCountry,ToCity,ToState,ToZip,ToCountry) VALUES (DATETIME(\'now\',\'localtime\'),?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)');
+			$stmt = $this->db->prepare('INSERT INTO calls (DateCreated,CallSid,CallFrom,CallTo,CallStatus,Direction) VALUES (DATETIME(\'now\',\'localtime\'),?,?,?,?,?)');
 			$vars=array($CallSid,$AccountSid,$CallFrom,$CallTo,$CallStatus,$ApiVersion,$Direction,$FromCity,$FromState,$FromZip,$FromCountry,$ToCity,$ToState,$ToZip,$ToCountry);
 			$stmt->execute($vars);
 		}
 
 		function save_dialed_call() {
-			$CallSid = $_REQUEST['CallSid'];
-			$DialCallSid=$_REQUEST['DialCallSid'];
-			$DialCallDuration=$_REQUEST['DialCallDuration'];
-			$DialCallStatus=$_REQUEST['DialCallStatus'];
-			$RecordingUrl=$_REQUEST['RecordingUrl'];
+			$CallSid = $_REQUEST['DialALegUUID'];
+			$DialCallSid=$_REQUEST['DialBlegUUID'];
+			$DialCallStatus=$_REQUEST['DialHangupCause'];
 
-			$stmt = $this->db->prepare('UPDATE calls set DialCallSid=?, DialCallDuration=?, DialCallStatus=?, RecordingUrl=? WHERE CallSid=?');
-			$stmt->execute(array($DialCallSid, $DialCallDuration,$DialCallStatus, $RecordingUrl, $CallSid));
+			$stmt = $this->db->prepare('UPDATE calls set DialCallSid=?, DialCallStatus=? WHERE CallSid=?');
+			$stmt->execute(array($DialCallSid, $DialCallStatus, $CallSid));
 		}
 	
 		function get_calls(){
