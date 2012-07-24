b61c03ee456aa4c71f4b4bef5aa6f857cf4bdc7f
tkosumi <tkosumi@gmail.com>
2012-07-24 16:51:56 +0900

    modified:   include/config.php

--- include/config.php
+++ include/config.php
@@ -1,7 +1,7 @@
 <?php
 	require "fullcourt.php";
 	require "db.php";
-	define('AGENT_NUMBER', '818043559221'); //set this to the phone number you'd like calls forwarded to
+	define('AGENT_NUMBER', '81456240616'); //set this to the phone number you'd like calls forwarded to
 	$ApiVersion = "0.1";
 	$AccountSid = "FC20f8676ba75a63ba452784ca5858d217";
 	$AuthToken = "ocYw1LU8A9w64uztN5dKPzwrNH9ynmuXtx";
@@ -17,5 +17,5 @@
 		else
 			return $phone;
 	}
-	
+
 ?>
