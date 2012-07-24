cf734f9b3a795502cbc3967ba6d1f34d158f7362
tkosumi <tkosumi@gmail.com>
2012-07-24 20:44:51 +0900

    modified:   handle_incoming_call.php

--- handle_incoming_call.php
+++ handle_incoming_call.php
@@ -2,7 +2,7 @@
 	require_once('./include/config.php');
 	header('Content-type: text/xml');
 	$db = new DB();
-	$db->saved_call();
+	$db->save_call();
 ?>
 <Response>
 	<Dial callbackUrl="http://dev.fullcourt.co/FullCourt-Call-Tracking/record_call.php" method="POST" callerId="815058381227">
